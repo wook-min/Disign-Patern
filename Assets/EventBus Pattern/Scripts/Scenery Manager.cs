4b4683765d84ff01623a4060d328c29f0f059f7a
wook-min <gaemicgr@gmail.com>
2025-10-17 21:44:54 +0900

    (2025.10.17) 이벤트 버스 패턴
    유니티2 강의3
    이벤트 버스 패턴 배우는중
    씬 이동에 관한 내용(Scene Manager)
    동기, 비동기 전환
    LoadScene
    LoadSceneAsync << 비동기는 코루틴 필요

---
+++ Assets/EventBus Pattern/Scripts/Scenery Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneryManager : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] GameObject screen;
+
+    private void Awake()
+    {
+        slider = GetComponentInChildren<Slider>();
+    }
+
+    public void LoadScene(int buildIndex)
+    {
+        
+    }
+
+    /*
+    public IEnumerator TranstionScene(int index)
+    {
+        slider.value = 0;
+        screen.SetActive(true);
+
+        // <AsyncOperation>
+        // - allowSceneActivation
+        // 장면이 준비된 즉시 장면이 활성화되는 것을 허용하는 변수입니다.
+    }
+    */
+}
