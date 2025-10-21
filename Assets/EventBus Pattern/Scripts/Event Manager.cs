8a17e67d46b10178f963751f32093e1461df43f9
wook-min <gaemicgr@gmail.com>
2025-10-21 21:43:54 +0900

    (2025.10.21) 전략 패턴
    유니티2 강의4
    이벤트 버스 구독, 해제 실행 함수
    간단한 애니메이션 제작
    ui 에셋 받기까지

---
+++ Assets/EventBus Pattern/Scripts/Event Manager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Condition
+{
+    START,
+    PAUSE,
+    FINISH,
+}
+
+public static class EventManager
+{
+
+    private static Dictionary<Condition, Action> dictionary = new();
+
+    // 이벤트 등록용 함수
+    public static void Subscribe(Condition condition, Action action)
+    {
+        // key가 있다면 event 추가 : +=
+        if (dictionary.ContainsKey(condition))
+        {
+            dictionary[condition] += action;
+        }
+        // key 가 없다면 event 등록 : =
+        else
+        {
+            dictionary.Add(condition, action);
+            // 또는 dictionary[condition] = action;
+        }
+        // OnEnable에 등록, OnDisable에 해제
+    }
+
+    // 이벤트 해제용 함수
+    public static void UnSubscribe(Condition condition, Action action)
+    {
+        if (dictionary.ContainsKey(condition))
+        {
+            if (dictionary.ContainsValue(action))
+            {
+                dictionary[condition] -= action;
+            }
+            else
+            {
+                Debug.LogError($"VALUE {action} doesn't exist");
+                return;
+            }
+        }
+        else
+        {
+            Debug.LogError($"KEY {condition} doesn't exist");
+            return;
+        }
+    }
+
+    public static void Publish(Condition condition)
+    {
+        if (dictionary.TryGetValue(condition, out Action action))
+        {
+            action?.Invoke();
+        }
+        else
+        {
+            Debug.LogError($"{condition} doesn't exist");
+            return;
+        }
+    }
+}
--- Assets/EventBus Pattern/Scripts/Scenery Manager.cs
+++ Assets/EventBus Pattern/Scripts/Scenery Manager.cs
@@ -5,28 +5,72 @@ using UnityEngine.UI;
 
 public class SceneryManager : MonoBehaviour
 {
+    [SerializeField] static SceneryManager instance;
     [SerializeField] Slider slider;
     [SerializeField] GameObject screen;
 
     private void Awake()
     {
-        slider = GetComponentInChildren<Slider>();
+        if (instance == null)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(instance);
+        }
     }
 
     public void LoadScene(int buildIndex)
     {
-        
+        screen.SetActive(true);
+        StartCoroutine(TranstionScene(buildIndex));
     }
 
-    /*
+    // 웬만하면 씬은 이넘으로 작업하기
     public IEnumerator TranstionScene(int index)
     {
         slider.value = 0;
-        screen.SetActive(true);
 
         // <AsyncOperation>
         // - allowSceneActivation
         // 장면이 준비된 즉시 장면이 활성화되는 것을 허용하는 변수입니다.
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+
+        asyncOperation.allowSceneActivation = false;
+        // fake Load Scene을 위해 자동으로 씬이 넘어가는 것을 방지
+
+        // <AsyncOperation>
+        // - isDone
+        // 해당 동작이 완료되었는지 나타내는 변수입니다. (읽기 전용)
+        while(asyncOperation.isDone == false)
+        {
+            // <AsyncOperation>
+            // - progress
+            // 작업의 진행상태를 나타내는 변수입니다. (읽기 전용)
+           
+
+            if (asyncOperation.progress >= 0.9f)
+            {
+                slider.value = Mathf.Lerp(slider.value, 1.0f, Time.deltaTime);
+
+                if (slider.value >= 0.99f)
+                {
+                    asyncOperation.allowSceneActivation = true;
+                }
+            }
+            else
+            {
+                slider.value = asyncOperation.progress;
+            }
+            
+            yield return null;
+        }
+
+        screen.SetActive(false);
+
     }
-    */
+    
 }
